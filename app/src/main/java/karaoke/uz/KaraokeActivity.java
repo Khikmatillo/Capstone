30fcc22b0d629c2063a14e53ad88a44fe4698fe2
Akhrorjon <a.aliev@student.inha.uz>
2017-09-19 10:46:42 +0900

    Axror's first commit

---
+++ app/src/main/java/karaoke/uz/KaraokeActivity.java
@@ -0,0 +1,15 @@
+package karaoke.uz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import uz.music.capstone.R;
+
+public class KaraokeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_karaoke);
+    }
+}
