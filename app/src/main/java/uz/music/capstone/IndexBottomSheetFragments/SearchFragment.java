243abf05128475d0ef3d212388287ffd6960607d
Maestro <azamat.abduazimov01@gmail.com>
2017-11-27 04:24:54 +0900

    By Azamat

--- app/src/main/java/uz/music/capstone/IndexBottomSheetFragments/SearchFragment.java
+++ app/src/main/java/uz/music/capstone/IndexBottomSheetFragments/SearchFragment.java
@@ -2,19 +2,22 @@ package uz.music.capstone.IndexBottomSheetFragments;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import uz.music.capstone.R;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class SearchFragment extends Fragment {
 
-
+    ListView searchlistview;
     public SearchFragment() {
         // Required empty public constructor
     }
@@ -23,8 +26,15 @@ public class SearchFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+       return inflater.inflate(R.layout.fragment_search, container, false);
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+
+    }
 }
