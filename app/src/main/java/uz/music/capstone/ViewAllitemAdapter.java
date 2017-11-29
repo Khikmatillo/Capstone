6c9591a58bb765cb60c1da8e626bbb9558a2b21f
Maestro <azamat.abduazimov01@gmail.com>
2017-11-30 04:33:33 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/res/layout/activity_nowplaying.xml

---
+++ app/src/main/java/uz/music/capstone/ViewAllitemAdapter.java
@@ -0,0 +1,115 @@
+package uz.music.capstone;
+
+/**
+ * Created by Maestro on 11/30/2017.
+ */
+
+import android.content.Context;
+import android.support.v7.widget.PopupMenu;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+/**
+ * Created by Ravi Tamada on 18/05/16.
+ */
+public class ViewAllitemAdapter extends RecyclerView.Adapter<ViewAllitemAdapter.MyViewHolder> {
+
+    private Context mContext;
+    private List<ViewAllSingleItem> albumList;
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView title, detail;
+        public ImageView thumbnail;
+        //public ImageView overflow;
+
+        public MyViewHolder(View view) {
+            super(view);
+            title = (TextView) view.findViewById(R.id.title);
+            detail = (TextView) view.findViewById(R.id.detail);
+            thumbnail = (ImageView) view.findViewById(R.id.thumbnail);
+           // overflow = (ImageView) view.findViewById(R.id.overflow);
+        }
+    }
+
+
+    public ViewAllitemAdapter(Context mContext, List<ViewAllSingleItem> albumList) {
+        this.mContext = mContext;
+        this.albumList = albumList;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.activity_view_all_single_item, parent, false);
+
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(final MyViewHolder holder, int position) {
+        ViewAllSingleItem album = albumList.get(position);
+        holder.title.setText(album.getName());
+        holder.detail.setText(album.getDetail());
+
+        // loading album cover using Glide library
+        Glide.with(mContext).load(album.getThumbnail()).into(holder.thumbnail);
+
+//        holder.overflow.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                showPopupMenu(holder.overflow);
+//            }
+//        });
+    }
+
+    /**
+     * Showing popup menu when tapping on 3 dots
+     */
+//    private void showPopupMenu(View view) {
+//        // inflate menu
+//        PopupMenu popup = new PopupMenu(mContext, view);
+//        MenuInflater inflater = popup.getMenuInflater();
+//        inflater.inflate(R.menu.menu_album, popup.getMenu());
+//        popup.setOnMenuItemClickListener(new MyMenuItemClickListener());
+//        popup.show();
+//    }
+
+    /**
+     * Click listener for popup menu items
+     */
+//    class MyMenuItemClickListener implements PopupMenu.OnMenuItemClickListener {
+//
+//        public MyMenuItemClickListener() {
+//        }
+//
+//        @Override
+//        public boolean onMenuItemClick(MenuItem menuItem) {
+//            switch (menuItem.getItemId()) {
+//                case R.id.action_add_favourite:
+//                    Toast.makeText(mContext, "Add to favourite", Toast.LENGTH_SHORT).show();
+//                    return true;
+//                case R.id.action_play_next:
+//                    Toast.makeText(mContext, "Play next", Toast.LENGTH_SHORT).show();
+//                    return true;
+//                default:
+//            }
+//            return false;
+//        }
+//    }
+
+    @Override
+    public int getItemCount() {
+        return albumList.size();
+    }
+}
